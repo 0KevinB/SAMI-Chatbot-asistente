e0ffb301d2fed54511116570462b2fb178d54e80
carolaljime21 <carolalvaradojime@gmail.com>
2024-12-04 11:13:11 -0500

    Creacion de vistas: login, dashboard principal, vista del paciente, historial del paciente, y componentes: slidebar

---
+++ frontend/SAMI/src/app/views/historial-clinico-paciente/historial-clinico-paciente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-historial-clinico-paciente',
+  imports: [],
+  templateUrl: './historial-clinico-paciente.component.html',
+  styleUrl: './historial-clinico-paciente.component.css'
+})
+export class HistorialClinicoPacienteComponent {
+
+}
---
+++ frontend/SAMI/src/app/views/login-medico/login-medico.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-medico',
+  imports: [],
+  templateUrl: './login-medico.component.html',
+  styleUrl: './login-medico.component.css'
+})
+export class LoginMedicoComponent {
+
+}
