331bd103a4a4928f8d2e7bddff83bdd8881e150d
Kevin <kabarrazueta@utpl.edu.ec>
2024-12-05 12:57:49 -0500

    Login de usuaruismedicos

--- frontend/SAMI/src/app/views/login-medico/login-medico.component.ts
+++ frontend/SAMI/src/app/views/login-medico/login-medico.component.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './../../services/auth.service'; // Ajusta la ruta según tu estructura
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login-medico',
-  imports: [],
   templateUrl: './login-medico.component.html',
-  styleUrl: './login-medico.component.css',
-  standalone: true
+  styleUrls: ['./login-medico.component.css'],
+  standalone: true,
+  imports: [HttpClientModule, FormsModule], // Asegúrate de incluirlo aquí
+  providers: [AuthService], // Declara el servicio como proveedor
 })
 export class LoginMedicoComponent {
+  cedula = '';
+  password = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login(): void {
+    if (this.cedula && this.password) {
+      this.authService
+        .login({ cedula: this.cedula, password: this.password })
+        .subscribe(
+          (response) => {
+            const token = response.token;
+            this.authService.saveToken(token);
+
+            const decodedToken = this.authService.getDecodedToken();
+            if (decodedToken && decodedToken.role === 'medico') {
+              console.log('Acceso permitido, rol médico');
+              this.router.navigate(['/dashboard']);
+            } else {
+              console.error('Acceso denegado, rol no permitido');
+              alert('Solo los médicos pueden acceder');
+              this.authService.logout();
+            }
+          },
+          (error) => {
+            console.error('Error al iniciar sesión:', error);
+            alert('Credenciales incorrectas');
+          }
+        );
+    } else {
+      alert('Por favor, complete todos los campos');
+    }
+  }
 }
