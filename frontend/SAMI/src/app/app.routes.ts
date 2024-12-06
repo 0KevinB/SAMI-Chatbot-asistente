c4ed97223d813cc26f2375f7e37e522b2bb93a1f
carolaljime21 <carolalvaradojime@gmail.com>
2024-12-05 22:16:11 -0500

    Creación de ruta de dashboard principal (dashboard-principal), + creación de slidebar (falta modificar estilos, solo estructura)

--- frontend/SAMI/src/app/app.routes.ts
+++ frontend/SAMI/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { LoginMedicoComponent } from './views/login-medico/login-medico.component';
+import { DashboardPrincipalComponent } from './views/dashboard-principal/dashboard-principal.component';
 
 export const routes: Routes = [
     { path: '', component: LoginMedicoComponent },
-    { path: 'login-medico', component: LoginMedicoComponent }
+    { path: 'login-medico', component: LoginMedicoComponent },
+    { path: 'dashboard-principal', component: DashboardPrincipalComponent }
 ];
--- frontend/SAMI/src/app/views/dashboard-principal/dashboard-principal.component.ts
+++ frontend/SAMI/src/app/views/dashboard-principal/dashboard-principal.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { SlidebarComponent } from '../../components/slidebar/slidebar.component';
 
 @Component({
   selector: 'app-dashboard-principal',
-  imports: [],
+  imports: [SlidebarComponent],
   templateUrl: './dashboard-principal.component.html',
-  styleUrl: './dashboard-principal.component.css'
+  styleUrl: './dashboard-principal.component.css',
+  standalone: true
 })
 export class DashboardPrincipalComponent {
 
