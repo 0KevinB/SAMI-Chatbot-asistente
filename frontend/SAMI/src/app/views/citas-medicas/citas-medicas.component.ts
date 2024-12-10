a06fd7cf4bc8ebdc217d820a67f8bc7f26ebf99a
carolaljime21 <carolalvaradojime@gmail.com>
2024-12-09 23:37:09 -0500

    Estilos en slidebar y dashboard principal, no completo

--- frontend/SAMI/src/app/views/citas-medicas/citas-medicas.component.ts
+++ frontend/SAMI/src/app/views/citas-medicas/citas-medicas.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { FullCalendarModule } from '@fullcalendar/angular';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-citas-medicas',
   standalone: true,
-  imports: [FullCalendarModule, HttpClientModule],
+  imports: [],
   templateUrl: './citas-medicas.component.html',
   styleUrl: './citas-medicas.component.css',
 })
