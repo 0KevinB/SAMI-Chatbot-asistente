a9635929e47d60d031c5e40d84ece014d1e0c2a3
carolaljime21 <carolalvaradojime@gmail.com>
2024-12-10 15:06:59 -0500

    Slidebar terminado, gestion de pacientes terminado, calendario d citas medicas pendiente

--- frontend/SAMI/src/app/views/citas-medicas/citas-medicas.component.ts
+++ frontend/SAMI/src/app/views/citas-medicas/citas-medicas.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SlidebarComponent } from '../../components/slidebar/slidebar.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-citas-medicas',
   standalone: true,
-  imports: [],
+  imports: [SlidebarComponent, CommonModule, HttpClientModule],
   templateUrl: './citas-medicas.component.html',
   styleUrl: './citas-medicas.component.css',
 })
