1457e0008b96c88ce62203bd6a6caaa110a57c12
carolaljime21 <carolalvaradojime@gmail.com>
2024-12-11 12:24:33 -0500

    Interfaz de perfil de paciente estructurado, falta establecer los estilos

--- frontend/SAMI/src/app/views/historial-clinico-paciente/historial-clinico-paciente.component.ts
+++ frontend/SAMI/src/app/views/historial-clinico-paciente/historial-clinico-paciente.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { SlidebarComponent } from '../../components/slidebar/slidebar.component';
+import { pacientes } from './perfilpaciente.mock';
+
 
 @Component({
   selector: 'app-historial-clinico-paciente',
-  imports: [],
+  imports: [CommonModule, HttpClientModule, SlidebarComponent],
   templateUrl: './historial-clinico-paciente.component.html',
-  styleUrl: './historial-clinico-paciente.component.css'
+  styleUrl: './historial-clinico-paciente.component.css',
+  standalone: true
 })
 export class HistorialClinicoPacienteComponent {
-
+  listaPacientes = pacientes;
 }
