d61cbb2405d8a03d6287e8e6c50d72d7641906b1
Kevin <kabarrazueta@utpl.edu.ec>
2024-12-11 18:01:32 -0500

    Actualizacion de estilos en el perfil del paciente

--- frontend/SAMI/src/app/views/historial-clinico-paciente/historial-clinico-paciente.component.ts
+++ frontend/SAMI/src/app/views/historial-clinico-paciente/historial-clinico-paciente.component.ts
@@ -4,14 +4,20 @@ import { Component } from '@angular/core';
 import { SlidebarComponent } from '../../components/slidebar/slidebar.component';
 import { pacientes } from './perfilpaciente.mock';
 
-
 @Component({
   selector: 'app-historial-clinico-paciente',
   imports: [CommonModule, HttpClientModule, SlidebarComponent],
   templateUrl: './historial-clinico-paciente.component.html',
   styleUrl: './historial-clinico-paciente.component.css',
-  standalone: true
+  standalone: true,
 })
 export class HistorialClinicoPacienteComponent {
   listaPacientes = pacientes;
+  consultationRecords: any[] = [
+    {
+      date: '13-Ago-2023, 10:00 AM',
+      reason: 'EDEMA DE MIEMBRO INFERIOR IZQUIERDO',
+    },
+  ];
+  patient: any = pacientes[0];
 }
