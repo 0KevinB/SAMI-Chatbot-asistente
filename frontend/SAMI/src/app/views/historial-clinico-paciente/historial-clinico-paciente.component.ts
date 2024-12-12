0cda358baea9f67c155ffa8393353f89debf4f73
Kevin <kabarrazueta@utpl.edu.ec>
2024-12-12 17:39:57 -0500

    Formulario de agregacion de pacientes

--- frontend/SAMI/src/app/views/historial-clinico-paciente/historial-clinico-paciente.component.ts
+++ frontend/SAMI/src/app/views/historial-clinico-paciente/historial-clinico-paciente.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { SlidebarComponent } from '../../components/slidebar/slidebar.component';
-import { pacientes } from './perfilpaciente.mock';
+import { pacientes } from '../../mocks/perfilpaciente.mock';
 
 @Component({
   selector: 'app-historial-clinico-paciente',
--- frontend/SAMI/src/app/views/vista-paciente/vista-paciente.component.ts
+++ frontend/SAMI/src/app/views/vista-paciente/vista-paciente.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { SlidebarComponent } from '../../components/slidebar/slidebar.component';
+import { pacientes } from './../../mocks/perfilpaciente.mock';
 @Component({
   selector: 'app-vista-paciente',
-  imports: [],
+  imports: [CommonModule, HttpClientModule, SlidebarComponent],
   templateUrl: './vista-paciente.component.html',
-  styleUrl: './vista-paciente.component.css'
+  styleUrl: './vista-paciente.component.css',
+  standalone: true,
 })
 export class VistaPacienteComponent {
-
+  listaPacientes = pacientes;
+  consultationRecords: any[] = [
+    {
+      date: '13-Ago-2023, 10:00 AM',
+      reason: 'EDEMA DE MIEMBRO INFERIOR IZQUIERDO',
+    },
+  ];
+  patient: any = pacientes[0];
 }
